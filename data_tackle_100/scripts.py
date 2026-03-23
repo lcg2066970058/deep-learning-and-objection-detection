22382f1242edcac31d06f815174192cd6545e4f8
shiqi <2066970058@qq.com>
2026-03-23 14:39:38 +0800

    更新忽略文件规则

--- data_tackle_100/scripts.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-m = np.load('yournpyfile.npy')
-print(m)
--- mycrawler/image_scraper/image_scraper/items.py
+++
@@ -1,7 +0,0 @@
-import scrapy
-
-class ImageScraperItem(scrapy.Item):
-    category = scrapy.Field()
-    keyword = scrapy.Field()   # 新增：记录具体是哪个场景/关键词
-    image_urls = scrapy.Field()
-    images = scrapy.Field()
